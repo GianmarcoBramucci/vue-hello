3bf5120fce5203d22ed7f09b0fbb8a6772a7d96b
GianmarcoBramucci <giangian797@gmail.com>
2024-03-27 18:26:03 +0100

    test and exeperipent

--- js/main.js
+++ js/main.js
@@ -7,10 +7,21 @@ Vue.createApp({
     data(){
         return{
             message: 'hello word',
-            url : './img/LOL.jpg'
+            url : './img/LOL.jpg',
+            url2 : '',
+            show : false,
+            nome : 'ff'
         }
     },
     methods: {
-
+       saluta: function(){
+            return this.message + this.nome;        
+       },
+       random: function(min,max){
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+       },
+       randomUrl: function(src){
+            return src = `https://picsum.photos/id/${this.random(1,100)}/200/300`;
+       }
     }
 }).mount('#risposta')
