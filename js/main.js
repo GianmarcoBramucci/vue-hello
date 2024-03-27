a7c825dc787b310fbcb66abdadd9df677882a467
GianmarcoBramucci <giangian797@gmail.com>
2024-03-27 16:34:20 +0100

    add script

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-let input = document.getElementById('input73');
-let btn = document.getElementById('btn-submit37');
-let divRisposta = document.getElementById('risposta');
+// let input = document.getElementById('input73');
+// let btn = document.getElementById('btn-submit37');
+// let divRisposta = document.getElementById('risposta');
+
+
+Vue.createApp({
+    data(){
+        return{
+            message: 'hello word',
+            url : './img/LOL.jpg'
+        }
+    },
+    methods: {
+
+    }
+}).mount('#risposta')
